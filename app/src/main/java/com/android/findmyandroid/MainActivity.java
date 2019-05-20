62da7e143f747390d31c18221c69140f6002d38d
Tran Van Vinh <vinhyt98@gmail.com>
2019-05-20 19:47:21 +0700

    giao dien home

--- app/src/main/java/com/android/findmyandroid/MainActivity.java
+++ app/src/main/java/com/android/findmyandroid/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(this, EmailSendActivity.class));
+//        startActivity(new Intent(this, EmailSendActivity.class));
 //        startActivity(new Intent(this, AddReceivedEmail.class));
 //        startActivity(new Intent(this, MainSetting.class));
 //        startActivity(new Intent(this, SMSSettingActivity.class));
