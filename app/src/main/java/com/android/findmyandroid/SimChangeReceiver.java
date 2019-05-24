94ce7fad71e339e07e43dc7e36b6dfbf90dfefb9
ManhPP <33551849+ManhPP@users.noreply.github.com>
2019-05-24 12:43:52 +0700

    .

--- app/src/main/java/com/android/findmyandroid/SimChangeReceiver.java
+++ app/src/main/java/com/android/findmyandroid/SimChangeReceiver.java
@@ -4,7 +4,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.net.wifi.WifiManager;
+import android.os.CountDownTimer;
 import android.util.Log;
 
 import com.android.findmyandroid.model.Contact;
@@ -14,6 +20,7 @@ import com.android.findmyandroid.utils.ContactHandler;
 import com.android.findmyandroid.utils.LocationHandler;
 import com.android.findmyandroid.utils.RecordHandler;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -49,6 +56,24 @@ public class SimChangeReceiver extends SMSReceiver{
             intent1.putExtra("onTakeePickture", this);
             intent1.putExtra("isFront", true);
             context.startService(intent1);
+
+            //bao dong
+            try {
+                Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+                final Ringtone r = RingtoneManager.getRingtone(context.getApplicationContext(), notification);
+                new CountDownTimer(10000, 1000) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        r.play();
+                    }
+
+                    public void onFinish() {
+                    }
+                }.start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
