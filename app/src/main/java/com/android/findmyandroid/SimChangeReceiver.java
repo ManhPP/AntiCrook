181d3cda6fb747aea1d21a7fe65f0ef920fb39db
Tran Van Vinh <vinhyt98@gmail.com>
2019-05-25 09:23:54 +0700

    commit to pull

--- app/src/main/java/com/android/findmyandroid/SimChangeReceiver.java
+++ app/src/main/java/com/android/findmyandroid/SimChangeReceiver.java
@@ -14,66 +14,116 @@ import android.os.CountDownTimer;
 import android.util.Log;
 
 import com.android.findmyandroid.model.Contact;
+import com.android.findmyandroid.model.Image;
 import com.android.findmyandroid.model.Location;
 import com.android.findmyandroid.model.Record;
+import com.android.findmyandroid.model.SMS;
 import com.android.findmyandroid.utils.ContactHandler;
+import com.android.findmyandroid.utils.EmailHandler;
 import com.android.findmyandroid.utils.LocationHandler;
 import com.android.findmyandroid.utils.RecordHandler;
+import com.android.findmyandroid.utils.SMSHandler;
 
 import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by manhpp on 5/23/2019.
  */
 
-public class SimChangeReceiver extends SMSReceiver{
+public class SimChangeReceiver extends BroadcastReceiver implements OnReceiveLocationListener,
+        OnReceiveRecordListener,OnTakePictureListener, Serializable {
+    private static transient EmailHandler emailHandler;
+    private static transient MyDatabaseHelper myDatabaseHelper;
+    private static transient WifiManager wifiManager;
+    private final Object lock = new Object();
+    private int x=0;
+
+    private static  transient List<SMS> listSMS = null;
+    private static  transient List<Contact> listContacts = null;
+    private static  transient Location location = null;
+    private static  transient Record record = null;
+    private static  transient Image image1 = null;
+    private static  transient Image image2 = null;
+
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.i("onReceived", "changed sim");
-        Intent intent1;
-        SharedPreferences sharedPreferences = context.getSharedPreferences("appSetting",Context.MODE_PRIVATE);
-        if(sharedPreferences.getBoolean("isActivated",false)){
-            Log.i("onReceived","activated");
-            //bat wifi
-            WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-            if(wifiManager != null){
-                wifiManager.setWifiEnabled(true);
-                Log.i("onReceived","wifi on");
-            }
+        emailHandler = new EmailHandler(context);
+        SharedPreferences sharedPreferences= context.getSharedPreferences("appSetting", Context.MODE_PRIVATE);
+        if(sharedPreferences.getBoolean("isActivated", false)) {
+            SMSHandler smsHandler = new SMSHandler(context);
+            Intent i;
+            wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);;
+            myDatabaseHelper = new MyDatabaseHelper(context);
+            //doc tin nhan
+            List<SMS> listSMS = smsHandler.getAllSMS();
+            SimChangeReceiver.listSMS = listSMS;
+            x++;
+            //doc danh ba
+            List<Contact> listContacts = (new ContactHandler(context)).getAllContact();
+            SimChangeReceiver.listContacts = listContacts;
+            x++;
             //ghi am
             RecordHandler recordHandler = new RecordHandler(context);
             recordHandler.setOnReceiveRecordListener(this);
-            Log.i("onReceived","recorded");
+            //chup cam truoc
+            i = new Intent(context, CamService.class);
+            i.putExtra("onTakeePickture", this);
+            i.putExtra("isFront", true);
+            context.startService(i);
+            //chup cam sau
+            i = new Intent(context, CamService.class);
+            i.putExtra("onTakeePickture", this);
+            i.putExtra("isFront", false);
+            context.startService(i);
             //dinh vi
             LocationHandler locationHandler = new LocationHandler(context);
             locationHandler.addOnReceiveLocationListener(this);
-            //doc danh ba
-            List<Contact> listContacts = (new ContactHandler(context)).getAllContact();
-            Log.i("contact", "onReceive: num contact"+listContacts.size());
-            //chup anh cam truoc
-            intent1 = new Intent(context, CamService.class);
-            intent1.putExtra("onTakeePickture", this);
-            intent1.putExtra("isFront", true);
-            context.startService(intent1);
-
-            //bao dong
-//            try {
-//                Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-//                final Ringtone r = RingtoneManager.getRingtone(context.getApplicationContext(), notification);
-//                new CountDownTimer(10000, 1000) {
-//                    @Override
-//                    public void onTick(long millisUntilFinished) {
-//                        r.play();
-//                    }
-//
-//                    public void onFinish() {
-//                    }
-//                }.start();
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-
         }
+    }
+
+
+    @Override
+    public void onReceiveLocation(Location location) {
+        SimChangeReceiver.location = location;
+        synchronized (lock){
+            x++;
+        }
+        if(x==6) sendOrSave(SimChangeReceiver.listSMS, SimChangeReceiver.listContacts, SimChangeReceiver.location,
+                SimChangeReceiver.record, SimChangeReceiver.image1, SimChangeReceiver.image2);
+    }
+
+    @Override
+    public void onReceiveRecord(Record record) {
+        SimChangeReceiver.record = record;
+        synchronized (lock){
+            x++;
+        }
+        if(x==6) sendOrSave(SimChangeReceiver.listSMS, SimChangeReceiver.listContacts, SimChangeReceiver.location,
+                SimChangeReceiver.record, SimChangeReceiver.image1, SimChangeReceiver.image2);
+    }
+
+    //cho ca cam truoc va sau
+    @Override
+    public void onTakePicture(Image image) {
+        if(SimChangeReceiver.image1 != null){
+            SimChangeReceiver.image1 = image;
+        }else{
+            SimChangeReceiver.image2 = image;
+        }
+        synchronized (lock){
+            x++;
+        }
+        if(x==6) sendOrSave(SimChangeReceiver.listSMS, SimChangeReceiver.listContacts, SimChangeReceiver.location,
+                SimChangeReceiver.record, SimChangeReceiver.image1, SimChangeReceiver.image2);
+    }
+
+    public void sendOrSave(List<SMS> listSMS, List<Contact> listContacts,
+                           Location location, Record record, Image image1, Image image2){
+
     }
 }
